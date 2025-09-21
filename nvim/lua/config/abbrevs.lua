09f1861d15f5668c95ba94464fef48428ef649ad
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-09-21 19:23:19 +0700

    feat: abbreviations revamped

--- nvim/lua/config/abbrevs.lua
+++ nvim/lua/config/abbrevs.lua
@@ -1,43 +1,47 @@
 -- Commandline abbreviations
-vim.cmd.cabbrev("Wq", "wq")
-vim.cmd.cabbrev("Wqa", "wqa")
+local function command_mode_abbrevs()
+  vim.cmd.cabbrev("Wq", "wq")
+  vim.cmd.cabbrev("Wqa", "wqa")
 
-vim.cmd.cabbrev("W", "w")
-vim.cmd.cabbrev("Q", "q")
-vim.cmd.cabbrev("Qa", "qa")
-vim.cmd.cabbrev("Set", "set")
+  vim.cmd.cabbrev("W", "w")
+  vim.cmd.cabbrev("Q", "q")
+  vim.cmd.cabbrev("Qa", "qa")
+  vim.cmd.cabbrev("Set", "set")
+end
 
 -- Insert mode abbreviations
-local abbrevs = {
-  "reutrn : return",
-  "pacakge : package",
-  "destinatino : destination",
+local function insert_mode_abbrevs()
+  local path = vim.fn.stdpath("config") .. "/abbreviations.json"
 
-  "shuold : should",
-  "shoudl : should",
+  local abbrs = {}
 
-  "addres : address",
-  "adress : address",
+  -- Load abbrevs from file
+  local status_ok = pcall(function()
+    if vim.fn.filereadable(path) ~= 1 then
+      vim.notify("Abbreviations file not found: " .. path, vim.log.levels.WARN)
+      return
+    end
 
-  "sucess : success",
-  "sucessful : successful",
-  "sucessfully : successfully",
-  "sucessfull : successful",
+    local json = vim.fn.readfile(path)
+    abbrs = vim.fn.json_decode(table.concat(json, "\n"))
+  end)
 
-  "succes : success",
-  "succesful : successful",
-  "succesfully : successfully",
+  if not status_ok then
+    vim.notify("Failed to load abbreviations", vim.log.levels.ERROR)
+    return
+  end
 
-  "mokcs : mocks",
-  "mokc: mock",
+  -- Assign abbrevs
+  for word, typos in pairs(abbrs) do
+    if type(typos) == "string" then
+      typos = { typos }
+    end
 
-  "resposne : response",
-  "responses : responses",
-}
-
-for _, value in pairs(abbrevs) do
-  local lhs, rhs = unpack(vim.split(value, ":"))
-  lhs = vim.fn.trim(lhs)
-  rhs = vim.fn.trim(rhs)
-  vim.cmd.iabbrev(lhs, rhs)
+    for _, typo in ipairs(typos) do
+      vim.cmd.iabbrev({ args = { typo, word } })
+    end
+  end
 end
+
+insert_mode_abbrevs()
+command_mode_abbrevs()
